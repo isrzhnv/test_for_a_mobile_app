a853ecaeaf8d8bb70ed6ff384c4a9e340d64bb46
yuriiserzhanov <100586766+yuriiserzhanov@users.noreply.github.com>
2023-02-17 19:26:15 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,8 +1,9 @@
--- Define your Lua code as a function
--- local function myCodeFunction()
+"
+local function myCodeFunction()
   -- Your Lua code goes here
   print("Hello, world!")
 end
 
 -- Return the function
--- return myCodeFunction
+return myCodeFunction
+"
